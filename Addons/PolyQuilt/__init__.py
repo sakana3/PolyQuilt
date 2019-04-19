0f09b1ad14060e7050094811186edddf0fbee181
sakana3 <nosaka_online@hotmail.com>
2019-04-20 03:37:31 +0900

    バージョンインクリメント

--- Addons/PolyQuilt/__init__.py
+++ Addons/PolyQuilt/__init__.py
@@ -14,7 +14,7 @@
 bl_info = {
     "name" : "PolyQuilt",
     "author" : "Sakana3",
-    "version": (0, 0,1),
+    "version": (0, 0,2),
     "blender" : (2, 80, 0),
     "location": "View3D > Mesh > PolyQuilt",
     "description": "Lowpoly Tool",
